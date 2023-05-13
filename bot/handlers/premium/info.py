816baf41ad11ea9ee53160d364a34708c3c70bc3
AccursedGalaxy <robinbohrer7@gmail.com>
2023-05-13 23:12:52 +0200

    added /info command

---
+++ bot/handlers/premium/info.py
@@ -0,0 +1,92 @@
+import logging
+import ccxt
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+import ta
+import os
+import requests
+from datetime import datetime, timedelta
+from telegram import Update
+from telegram.ext import CallbackContext
+from bot.utils import log_command_usage
+from config.settings import LUNARCRUSH_API_KEY
+from bot.utils import restricted, PlotChart
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class InfoHandler:
+    SYMBOL_MAPPING = {
+        "BTC": 1,
+        "ETH": 2,
+        # Add more mappings as needed
+    }
+
+    @staticmethod
+    def get_coin_info(symbol):
+        if symbol in InfoHandler.SYMBOL_MAPPING:
+            coin_id = InfoHandler.SYMBOL_MAPPING[symbol]
+        else:
+            logger.error("Symbol mapping not found")
+            return None
+
+        url = f"https://lunarcrush.com/api3/coins/{coin_id}"
+        headers = {"Authorization": f"Bearer {LUNARCRUSH_API_KEY}"}
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.RequestException as e:
+            logger.exception("Connection error while fetching coin info from LunarCrush API")
+            return None
+
+        if "data" in data:
+            coin_data = data["data"]
+            return coin_data
+        else:
+            logger.error("Error in LunarCrush API response: Required data not found")
+            return None
+
+    @staticmethod
+    @restricted
+    @log_command_usage
+    def get_coin_info_command(update: Update, context: CallbackContext):
+        # Get the user's input
+        symbol = context.args[0]  # Assuming the symbol is passed as a command argument
+
+        # Fetch coin info
+        coin_data = InfoHandler.get_coin_info(symbol)
+        if coin_data is None:
+            update.message.reply_text("Error fetching coin info. Please try again later.")
+            return
+
+        # Extract relevant information
+        name = coin_data.get("name")
+        symbol = coin_data.get("symbol")
+        price = coin_data.get("price")
+        percent_change_24h = coin_data.get("percent_change_24h")
+        percent_change_7d = coin_data.get("percent_change_7d")
+        percent_chagne_30d = coin_data.get("percent_change_30d")
+        market_cap = coin_data.get("market_cap")
+        sentiment_relative = coin_data.get("sentiment_relative")
+
+        # Generate the response message
+        message = f"""Coin Info:
+        Symbol: {symbol}
+        Name: {name}
+        Price: {price}
+        24h % Change: {percent_change_24h}
+        7d % Change: {percent_change_7d}
+        30d % Change: {percent_chagne_30d}
+        Market Cap: {market_cap}
+        Bullish Sentiment %: {sentiment_relative}"""
+
+        # Plot chart
+        chart_file = PlotChart.plot_ohlcv_chart(symbol)
+
+        # Send chart and info as a reply
+        update.message.reply_photo(open(chart_file, 'rb'), caption=message)
+
--- bot/utils.py
+++ bot/utils.py
@@ -2,6 +2,16 @@ from functools import wraps
 from telegram import Update
 from telegram.ext import CallbackContext
 from users.management import check_user_access
+import ccxt
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+import ta
+import os
+from datetime import datetime, timedelta
+import requests
+from config.settings import LUNARCRUSH_API_KEY
+
 
 import functools
 from bot.database import Session, CommandUsage
@@ -45,3 +55,60 @@ def log_command_usage(func):
         return func(update, context, *args, **kwargs)
 
     return wrapper
+
+class PlotChart:
+    def plot_ohlcv_chart(symbol):
+        # Fetch OHLCV data from Binance
+        exchange = ccxt.bybit()
+        ohlcv = exchange.fetch_ohlcv(symbol.upper() + '/USDT', '4h')
+
+        # Filter data to display only the last 4 weeks
+        two_weeks_ago = datetime.now() - timedelta(weeks=4)
+        ohlcv = [entry for entry in ohlcv if datetime.fromtimestamp(entry[0] // 1000) >= two_weeks_ago]
+
+        # Convert timestamp to datetime objects
+        for entry in ohlcv:
+            entry[0] = datetime.fromtimestamp(entry[0] // 1000)
+
+        # Create a DataFrame and set 'Date' as the index
+        df = pd.DataFrame(ohlcv, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        df.set_index('Date', inplace=True)
+
+        # Add RSI
+        delta = df['Close'].diff()
+        gain, loss = delta.where(delta > 0, 0), delta.where(delta < 0, 0).abs()
+        avg_gain = gain.rolling(window=14).mean()
+        avg_loss = loss.rolling(window=14).mean()
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+        df['RSI'] = rsi
+
+        # Add moving averages
+        df['SMA21'] = ta.trend.sma_indicator(df['Close'], window=21)
+        df['SMA50'] = ta.trend.sma_indicator(df['Close'], window=50)
+
+        # Create a Plotly figure
+        fig = go.Figure()
+
+        # Add OHLCV data
+        fig.add_trace(go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name='Price'))
+
+        # Add moving averages
+        fig.add_trace(go.Scatter(x=df.index, y=df['SMA21'], mode='lines', name='SMA21', line=dict(color='orange', width=1)))
+        fig.add_trace(go.Scatter(x=df.index, y=df['SMA50'], mode='lines', name='SMA50', line=dict(color='blue', width=1)))
+
+        # Customize the layout
+        fig.update_layout(
+            title=f'{symbol} OHLCV Chart',
+            xaxis=dict(type='date', tickformat="%H:%M %b-%d", tickmode='auto', nticks=10, rangeslider=dict(visible=False)),
+            yaxis=dict(title='Price (USDT)'),
+            legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1),
+            template='plotly_dark',
+            margin=dict(b=40, t=40, r=40, l=40)
+        )
+
+        # Save the chart as a PNG image
+        chart_file = f"charts/{symbol}_chart.png"
+        fig.write_image(chart_file, scale=1.5, width=1000, height=600)
+
+        return chart_file
