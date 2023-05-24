34f08794802f0e3ac54002e80c2640b3ab798d9c
AccursedGalaxy <robinbohrer7@gmail.com>
2023-05-24 15:12:50 +0200

    error handling and symbol checking

--- bot/scripts/price_alerts.py
+++ bot/scripts/price_alerts.py
@@ -6,6 +6,9 @@ from telegram.ext import CallbackContext
 # setup database
 from bot.database import PriceAlertRequest, Session
 
+# setup logging
+import logging
+logger = logging.getLogger(__name__)
 
 class PriceAlerts:
     def check_price_alerts(context: CallbackContext):
@@ -13,9 +16,13 @@ class PriceAlerts:
         price_alert_requests = session.query(PriceAlertRequest).all()
 
         for price_alert_request in price_alert_requests:
-            exchange = ccxt.bybit()
-            ticker = exchange.fetch_ticker(price_alert_request.symbol)
-            current_price = ticker['last']
+            try:
+                exchange = ccxt.bybit()
+                ticker = exchange.fetch_ticker(price_alert_request.symbol)
+                current_price = ticker['last']
+            except ccxt.BaseError as e:
+                logger.error(f"Failed to fetch ticker for symbol {price_alert_request.symbol}: {e}")
+                continue
 
             # If the current price is within 0.5% of the price alert request, send a message to the user
             if current_price >= price_alert_request.price_level * Decimal('0.995') and current_price <= price_alert_request.price_level * Decimal('1.005'):
