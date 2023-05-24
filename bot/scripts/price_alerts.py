093d2469de32305a77ef80fec1961e910692c902
AccursedGalaxy <robinbohrer7@gmail.com>
2023-05-24 15:22:54 +0200

    to_delete price alerts

--- bot/scripts/price_alerts.py
+++ bot/scripts/price_alerts.py
@@ -15,14 +15,11 @@ class PriceAlerts:
         session = Session()
         price_alert_requests = session.query(PriceAlertRequest).all()
 
+        to_delete = []
         for price_alert_request in price_alert_requests:
-            try:
-                exchange = ccxt.bybit()
-                ticker = exchange.fetch_ticker(price_alert_request.symbol)
-                current_price = ticker['last']
-            except ccxt.BaseError as e:
-                logger.error(f"Failed to fetch ticker for symbol {price_alert_request.symbol}: {e}")
-                continue
+            exchange = ccxt.bybit()
+            ticker = exchange.fetch_ticker(price_alert_request.symbol)
+            current_price = ticker['last']
 
             # If the current price is within 0.5% of the price alert request, send a message to the user
             if current_price >= price_alert_request.price_level * Decimal('0.995') and current_price <= price_alert_request.price_level * Decimal('1.005'):
@@ -31,6 +28,10 @@ class PriceAlerts:
                     text=f"🔔 Price Alert! 🔔\n\nThe price of {price_alert_request.symbol} has reached your set level of {price_alert_request.price_level}. The current price is now: {current_price}."
                 )
 
-                # Delete the price alert request from the database
-                session.delete(price_alert_request)
-                session.commit()
+                # Add the price alert request to the list of requests to be deleted
+                to_delete.append(price_alert_request)
+
+        # Delete all the price alert requests in the list
+        for alert in to_delete:
+            session.delete(alert)
+        session.commit()
