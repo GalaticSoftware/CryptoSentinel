e9700066cc8b552b5497b4cab3f74a0c66656f79
AccursedGalaxy <robinbohrer7@gmail.com>
2023-05-28 17:40:52 +0200

    better formatting for whatsup

--- bot/handlers/free/whatsup.py
+++ bot/handlers/free/whatsup.py
@@ -2,6 +2,8 @@ import requests
 from telegram import Update
 from telegram.ext import CallbackContext
 import logging
+import datetime
+from datetime import datetime
 
 from bot.utils import log_command_usage
 from config.settings import LUNARCRUSH_API_KEY
@@ -10,8 +12,6 @@ from config.settings import LUNARCRUSH_API_KEY
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# WhatsupHandler class definition
-# This class contains the implementation of the /whatsup command handler.
 class WhatsupHandler:
     """
     Handles the /whatsup command, which fetches the top URLs engagement data
@@ -39,15 +39,20 @@ class WhatsupHandler:
             response_data = response.json()
 
             # Log the response data for debugging
-            logger.info("Response data from LunarCrush API: %s", response_data)
+            logger.debug("Response data from LunarCrush API: %s", response_data)
 
             # Check if the response data is generated successfully
             if response_data['config']['generated']:
                 # Process and format the data
                 coins_data = response_data['data']['coins']['urls_engagement']
-                message = "Top URLs engagement:\n\n"
+                message = "🚀 Top URLs Engagement for Cryptocurrencies 🚀\n\n"
                 for coin in coins_data:
-                    message += f"{coin['meta']['title']}: {coin['meta']['url']}\n"
+                    # Convert timestamp to human-readable format
+                    timestamp = datetime.fromtimestamp(coin['meta']['time'])
+                    message += f"🔗 Title: {coin['meta']['title']}\n"
+                    message += f"🌐 URL: {coin['meta']['url']}\n"
+                    message += f"📈 Engagement Value: {coin['value']}\n"
+                    message += f"🕒 Time: {timestamp.strftime('%Y-%m-%d %H:%M:%S')}\n\n"
 
                 # Log the formatted message for debugging
                 logger.info("Formatted message: %s", message)
@@ -56,14 +61,14 @@ class WhatsupHandler:
                 update.message.reply_text(message)
             else:
                 # Notify the user if there's an error fetching data from LunarCrush API
-                update.message.reply_text("Error fetching data from LunarCrush API.")
+                update.message.reply_text("Error fetching data from LunarCrush API. Please try again later.")
                 logger.error("Error fetching data from LunarCrush API. Response data: %s", response_data)
         except requests.exceptions.HTTPError as http_err:
             logger.error(f'HTTP error occurred: {http_err}')
-            update.message.reply_text("An HTTP error occurred while processing the /whatsup command.")
+            update.message.reply_text("An HTTP error occurred while processing the /whatsup command. Please try again later.")
         except requests.exceptions.RequestException as req_err:
             logger.error(f'Request error occurred: {req_err}')
-            update.message.reply_text("A network error occurred while processing the /whatsup command.")
+            update.message.reply_text("A network error occurred while processing the /whatsup command. Please check your connection and try again.")
         except Exception as e:
             logger.exception("An error occurred while processing the /whatsup command: %s", e)
-            update.message.reply_text("An error occurred while processing the /whatsup command.")
+            update.message.reply_text("An unexpected error occurred while processing the /whatsup command. Please try againlater.")
