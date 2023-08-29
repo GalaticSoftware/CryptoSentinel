83ece228aa2d50b5e7ab08a759ca60965027e60a
accursedgalaxy <robinbohrer7@gmail.com>
2023-08-29 13:26:12 +0200

    changed exchnage from bybit to coinbasepro

--- bot/scripts/alerts.py
+++ bot/scripts/alerts.py
@@ -21,7 +21,7 @@ class PriceAlerts:
 
         to_delete = []
         for price_alert_request in price_alert_requests:
-            exchange = ccxt.bybit()
+            exchange = ccxt.coinbasepro()
             ticker = exchange.fetch_ticker(price_alert_request.symbol)
             current_price = ticker["last"]
 
